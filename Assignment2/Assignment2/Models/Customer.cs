4d8abaaae65515f1699adacbc3eed5e93f6a2ecb
Shina Qin <bqshina@gmail.com>
2024-01-18 22:59:58 +1100

    add class models

---
+++ Assignment2/Assignment2/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace McbaExample.Models;
+
+public class Customer
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int CustomerID { get; set; }
+
+    [Required, StringLength(50)]
+    public string Name { get; set; }
+
+    [StringLength(50)]
+    public string Address { get; set; }
+
+    [StringLength(40)]
+    public string City { get; set; }
+
+    [StringLength(4)]
+    public string PostCode { get; set; }
+
+    public virtual List<Account> Accounts { get; set; }
+}
