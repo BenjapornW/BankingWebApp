058b0efe3d200fb7bcb082d65e3e3a57908e1349
Shina Qin <bqshina@gmail.com>
2024-01-20 01:32:40 +1100

    load json

---
+++ Assignment2/Assignment2/Data/McbaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using McbaExample.Models;
+
+namespace Assignment2.Data
+{
+	public class McbaContext: DbContext
+    {
+        public McbaContext(DbContextOptions<McbaContext> options) : base(options)
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Login> Logins { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
+
