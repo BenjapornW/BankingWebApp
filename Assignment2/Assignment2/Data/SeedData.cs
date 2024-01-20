eb99d513a0b43fecd791c5bcfacbb071011239c3
Shina Qin <bqshina@gmail.com>
2024-01-20 17:43:57 +1100

    create tables

--- Assignment2/Assignment2/Data/SeedData.cs
+++ Assignment2/Assignment2/Data/SeedData.cs
@@ -5,8 +5,8 @@ using System.Transactions;
 
 namespace Assignment2.Data
 {
-	public static class SeedData
-	{
+    public static class SeedData
+    {
         public static void Initialize(IServiceProvider serviceProvider)
         {
             var context = serviceProvider.GetRequiredService<McbaContext>();
@@ -17,20 +17,21 @@ namespace Assignment2.Data
         }
     }
 
-    public static async List<Customer> GetCustomer()
-    {
-        // Check if any people already exist and if they do, stop.
-        const string Url = "https://coreteaching01.csit.rmit.edu.au/~e103884/wdt/services/customers/";
+    //public static async List<Customer> GetCustomer()
+    //{
+    //    // Check if any people already exist and if they do, stop.
+    //    const string Url = "https://coreteaching01.csit.rmit.edu.au/~e103884/wdt/services/customers/";
 
-        // Contact webservice.
-        using var client = new HttpClient();
-        var json = await client.GetStringAsync(Url);
+    //    // Contact webservice.
+    //    using var client = new HttpClient();
+    //    var json = await client.GetStringAsync(Url);
 
-        // Convert JSON into objects.
-        var customers = JsonConvert.DeserializeObject<List<Customer>>(json, new JsonSerializerSettings
-        {
-            DateFormatString = "dd/MM/yyyy"
-        });
-        return customers;
-    }
+    //    // Convert JSON into objects.
+    //    var customers = JsonConvert.DeserializeObject<List<Customer>>(json, new JsonSerializerSettings
+    //    {
+    //        DateFormatString = "dd/MM/yyyy"
+    //    });
+    //    return customers;
+    //}
+}
 
--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -1,7 +1,18 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Assignment2.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<McbaContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Assignment2Context"));
+
+    // Enable lazy loading.
+    //options.UseLazyLoadingProxies();
+});
 
 var app = builder.Build();
 
