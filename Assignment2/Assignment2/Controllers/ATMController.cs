9f2ca57a7a45faddd13403776ece3952904c3a47
Shina Qin <bqshina@gmail.com>
2024-01-24 16:31:51 +1100

    finish deposit form and make all the transaction buttons work

--- Assignment2/Assignment2/Controllers/ATMController.cs
+++ Assignment2/Assignment2/Controllers/ATMController.cs
@@ -31,7 +31,12 @@ namespace Assignment2.Controllers
         //    return View();
         //}
 
-        public async Task<IActionResult> TransactionForm(int id) => View(await _context.Accounts.FindAsync(id));
+        public async Task<IActionResult> TransactionForm(int id, string actionType)  {
+
+            var account = await _context.Accounts.FindAsync(id);
+
+            return View(account);
+        }
 
         public async Task<IActionResult> Deposit(int id) => View(await _context.Accounts.FindAsync(id));
 
