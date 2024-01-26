d56c72c408da579954cbc71a3f662e6213ef1b83
BWOil <115447698+BWOil@users.noreply.github.com>
2024-01-26 22:54:19 +1100

    created BillPayController&Viewmodels

---
+++ Assignment2/Assignment2/ViewModels/BillPayViewmodels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Assignment2.Models;
+
+namespace Assignment2.ViewModels
+{
+    public class BillPaySummaryViewModel
+    {
+        public decimal CurrentMonthTotal { get; set; }
+        public decimal NextMonthTotal { get; set; }
+        public List<BillPay> UpcomingPayments { get; set; }
+    }
+}
+
+
