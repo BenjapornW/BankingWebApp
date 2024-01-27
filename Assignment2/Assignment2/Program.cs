3db38771c2901ba74be021e6152a0ac9cee1233c
Shina Qin <bqshina@gmail.com>
2024-01-27 17:21:02 +1100

    seed payees

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -73,15 +73,14 @@ app.UseHangfireDashboard();
 
 app.UseSession();
 
+app.MapDefaultControllerRoute();
+
 // reset monthly bills pay runs every minute
 //RecurringJob.AddOrUpdate<BillPayService>(x => x.UpdateMonthlyBillStatus(), "0 0 1 * *");
-RecurringJob.AddOrUpdate<BillPayService>(x => x.UpdateMonthlyBillStatus(), "0 */5 * * *");
+RecurringJob.AddOrUpdate<BillPayService>(x => x.UpdateMonthlyBillStatus(), "*/5 * * * * ");
 
 // Bill pay runs every minute
 RecurringJob.AddOrUpdate<BillPayService>(x => x.PayScheduledBills(), "* * * * *");
-
-
-app.MapDefaultControllerRoute();
 
 app.Run();
 
