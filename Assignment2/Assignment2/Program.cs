e9d3b8cf942e3d8fd454748d68b1b059f86aa1fc
BWOil <115447698+BWOil@users.noreply.github.com>
2024-01-27 16:37:47 +1100

    repushed

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -73,12 +73,12 @@ app.UseHangfireDashboard();
 
 app.UseSession();
 
-// reset monthly bills pay runs every minute
-//RecurringJob.AddOrUpdate<BillPayService>(x => x.UpdateMonthlyBillStatus(), "0 0 1 * *");
-RecurringJob.AddOrUpdate<BillPayService>(x => x.UpdateMonthlyBillStatus(), "0 */5 * * *");
+//// reset monthly bills pay runs every minute
+////RecurringJob.AddOrUpdate<BillPayService>(x => x.UpdateMonthlyBillStatus(), "0 0 1 * *");
+//RecurringJob.AddOrUpdate<BillPayService>(x => x.UpdateMonthlyBillStatus(), "0 */5 * * *");
 
-// Bill pay runs every minute
-RecurringJob.AddOrUpdate<BillPayService>(x => x.PayScheduledBills(), "* * * * *");
+//// Bill pay runs every minute
+//RecurringJob.AddOrUpdate<BillPayService>(x => x.PayScheduledBills(), "* * * * *");
 
 
 app.MapDefaultControllerRoute();
