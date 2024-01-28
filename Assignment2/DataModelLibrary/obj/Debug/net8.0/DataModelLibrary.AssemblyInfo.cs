bee68aa3b360f84efd79ef3f7fea2abf66bce723
BWOil <115447698+BWOil@users.noreply.github.com>
2024-01-29 03:06:40 +1100

    created MbcaWebAPi

--- Assignment2/DataModelLibrary/obj/Debug/net8.0/DataModelLibrary.AssemblyInfo.cs
+++ Assignment2/DataModelLibrary/obj/Debug/net8.0/DataModelLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataModelLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631e27f1f0e94946f357b40032049b3d12056d23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataModelLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataModelLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Assignment2/MbcaAdminWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MbcaAdminWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
