f374673765e5d0887973ed7205e8ec609beb90cd
BWOil <115447698+BWOil@users.noreply.github.com>
2024-01-29 14:47:11 +1100

    reconnected to login

--- Assignment2/MbcaAdminWeb/Program.cs
+++ Assignment2/MbcaAdminWeb/Program.cs
@@ -1,7 +1,32 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Net.Mime;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Configure api client.
+builder.Services.AddHttpClient("api", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5000");
+    client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
+});
+
+// Add controllers with views.
 builder.Services.AddControllersWithViews();
+
+// Add and configure session middleware.
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set the session timeout.
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -10,15 +35,17 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
+// Use Session Middleware
+app.UseSession();
+
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 
 app.Run();
 
