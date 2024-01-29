dfe90f841969a38ee093395eab692fe5b56fb62b
BWOil <115447698+BWOil@users.noreply.github.com>
2024-01-29 11:03:48 +1100

    updated logincontroller

--- Assignment2/MbcaAdminWeb/Controllers/CustomerController.cs
+++ Assignment2/MbcaAdminWeb/Controllers/CustomerController.cs
@@ -1,9 +1,36 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleHashing.Net;
+using DataModelLibrary.Models;
+using DataModelLibrary.Data;
+
 namespace MbcaAdminWeb.Controllers
 {
-	public class CustomerController
-	{
-		
-	}
+    [Route("/Mcba/Customer")]
+    public class CustomerController : Controller
+    {
+        private static readonly ISimpleHash s_simpleHash = new SimpleHash();
+
+        //private readonly McbaContext _context;
+        private readonly HttpClient _client;
+
+        //public LoginController(McbaContext context)
+        //{
+        //    _context = context;
+        //}
+
+        public CustomerController(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("api");
+        }
+
+        //public IActionResult Login() => View();
+
+    }
 }
+
+
+
+
+
+
 
