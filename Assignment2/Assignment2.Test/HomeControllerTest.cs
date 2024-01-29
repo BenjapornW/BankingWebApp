bfb893f30fc8a989ed071ad021902af2f946417b
BWOil <115447698+BWOil@users.noreply.github.com>
2024-01-29 22:00:59 +1100

    added homecontroller test

--- Assignment2/Assignment2.Test/HomeControllerTest.cs
+++ Assignment2/Assignment2.Test/HomeControllerTest.cs
@@ -1,10 +1,60 @@
-﻿namespace Assignment2.Test;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Assignment2.Controllers;
+using Assignment2.ViewModels;
+using Xunit;
+using Microsoft.Extensions.Logging;
+using Moq;
 
-public class UnitTest1
+namespace Assignment2.Test;
+
+public class HomeControllerTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly Mock<ILogger<HomeController>> _mockLogger;
 
+    public HomeControllerTests()
+    {
+        _mockLogger = new Mock<ILogger<HomeController>>();
+    }
+
+    [Fact]
+    public void Index_ReturnsAViewResult()
+    {
+        // Arrange
+        var controller = new HomeController(_mockLogger.Object);
+
+        // Act
+        var result = controller.Index();
+
+        // Assert
+        Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public void Privacy_ReturnsAViewResult()
+    {
+        // Arrange
+        var controller = new HomeController(_mockLogger.Object);
+
+        // Act
+        var result = controller.Privacy();
+
+        // Assert
+        Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public void Error_ReturnsAViewResult_WithAViewModel()
+    {
+        // Arrange
+        var controller = new HomeController(_mockLogger.Object);
+
+        // Act
+        var result = controller.Error();
+
+        // Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<ErrorViewModel>(viewResult.ViewData.Model);
+        Assert.NotNull(model.RequestId);
     }
 }
